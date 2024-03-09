ffe46c6d0e1860d9c2aecb4af7ad8a79ea4db64a
valebuyondo <buyondovale@gmail.com>
2024-03-09 21:46:54 +0300

    .\Notifications.js

---
+++ mvp2/src/components/Notifications.js
@@ -0,0 +1,14 @@
+// Notifications.js
+
+import React from 'react';
+
+const Notifications = () => {
+  return (
+    <div className="notifications-container">
+      <h2>Notifications</h2>
+      {/* Display notifications */}
+    </div>
+  );
+};
+
+export default Notifications;

03fff9c9a4034bd2ae173218fc7873a19db609bd
valebuyondo <buyondovale@gmail.com>
2024-03-09 21:45:06 +0300

    .\ViewAssets.js

---
+++ mvp2/src/components/ViewAssets.js
@@ -0,0 +1,14 @@
+// ViewAssets.js
+
+import React from 'react';
+
+const ViewAssets = () => {
+  return (
+    <div className="view-assets-container">
+      <h2>View Assets</h2>
+      {/* Display list of assets */}
+    </div>
+  );
+};
+
+export default ViewAssets;

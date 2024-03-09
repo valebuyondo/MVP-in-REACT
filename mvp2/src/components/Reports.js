5ca74af98c8299791cd53aa3e5b04e996e78adf5
valebuyondo <buyondovale@gmail.com>
2024-03-09 21:45:42 +0300

    .\Reports.js

---
+++ mvp2/src/components/Reports.js
@@ -0,0 +1,14 @@
+// Reports.js
+
+import React from 'react';
+
+const Reports = () => {
+  return (
+    <div className="reports-container">
+      <h2>Reports</h2>
+      {/* Display reports and analytics */}
+    </div>
+  );
+};
+
+export default Reports;

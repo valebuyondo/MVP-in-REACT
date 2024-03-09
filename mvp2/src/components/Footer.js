7b785032d456151248ed5f8ffb1b1943e29efd0e
valebuyondo <buyondovale@gmail.com>
2024-03-09 21:48:12 +0300

    .\Footer.js

---
+++ mvp2/src/components/Footer.js
@@ -0,0 +1,15 @@
+// Footer.js
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <div className="container">
+        <p>&copy; 2024 Asset Tracking System. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

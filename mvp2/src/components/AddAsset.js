4a2d9148fdf5a8e65ce5deccbc0e197329e75026
valebuyondo <buyondovale@gmail.com>
2024-03-09 21:44:26 +0300

    .\AddAsset.js

---
+++ mvp2/src/components/AddAsset.js
@@ -0,0 +1,14 @@
+// AddAsset.js
+
+import React from 'react';
+
+const AddAsset = () => {
+  return (
+    <div className="add-asset-container">
+      <h2>Add New Asset</h2>
+      {/* Add asset form */}
+    </div>
+  );
+};
+
+export default AddAsset;

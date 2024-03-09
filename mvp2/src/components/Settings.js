880841fd734ce9ec2b09cd227728bfa895f941db
valebuyondo <buyondovale@gmail.com>
2024-03-09 21:47:30 +0300

    .\Settings.js

---
+++ mvp2/src/components/Settings.js
@@ -0,0 +1,14 @@
+// Settings.js
+
+import React from 'react';
+
+const Settings = () => {
+  return (
+    <div className="settings-container">
+      <h2>Settings</h2>
+      {/* Display settings options */}
+    </div>
+  );
+};
+
+export default Settings;
